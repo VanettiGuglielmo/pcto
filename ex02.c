1dd78c29d37f698d5a10b68e8ff41681acb68edb
VanettiGuglielmo <128370169+VanettiGuglielmo@users.noreply.github.com>
2023-03-20 14:27:25 +0100

    Add files via upload

---
+++ ex02.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+   int x;  
+   printf("inserisci un valore: ");
+   scanf("%d", &x);
+   printf("x:%d\n", x);
+   printf("inserisci un altro numero: ");
+   scanf("%d", &x);
+   
+
+
+
+}
