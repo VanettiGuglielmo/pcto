6cc82b78bf7ba86cd0a337596c91bc5efad97228
VanettiGuglielmo <128370169+VanettiGuglielmo@users.noreply.github.com>
2023-03-20 15:33:45 +0100

    Add files via upload

---
+++ ex04.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+ 
+int x;
+printf("inserisci la tua età: ");
+scanf("%d",&x);
+if(x>18)
+{
+    prinf("accesso garantito");
+}
+else
+{
+    prinf("accesso non garantito")
+}
+}
