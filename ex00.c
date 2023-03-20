998844012c61143f0c3560564f5d6f29276ca6ae
VanettiGuglielmo <128370169+VanettiGuglielmo@users.noreply.github.com>
2023-03-20 12:36:12 +0100

    Add files via upload

---
+++ ex00.c
@@ -0,0 +1,12 @@
+//creare una variabile di tipo float, inizializzarla ad un valore a vostra scelta 
+//stampare la variabile 
+
+#include <stdio.h>
+
+int main()
+{
+    float o = 28;
+    char e = 'E';
+    char l = 'G';
+    printf("ho creato uanvariabile float %f, il primo carattere è %c, il secondo carattere è %c\n", o,e, l);
+}
