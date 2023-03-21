39979ae906d448dae1edafe9b238cf8b3285f598
VanettiGuglielmo <128370169+VanettiGuglielmo@users.noreply.github.com>
2023-03-21 10:22:53 +0100

    Add files via upload

---
+++ ex08.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+float x;
+float y;
+float z; 
+
+printf("inserire il primo lato");
+scanf("%f", &x);
+    printf("inserire il secondo lato");
+    scanf("%f", &y);
+
+prinf("inserire il terzo lato");
+scanf("%f", &z);
+if( x + y > z && y + z > x && x + z > y)
+{
+    printf("è un triangolo\n");
+    else if("x==y, && y==z, && z==x")
+printf("è equilatero")
+}
+else
+{
+if(" x==y, || y==z, || z==x");
+printf("è isoscele")
+{
+else if
+printf("è scaleno")
+}
+
+}
+ }
+
