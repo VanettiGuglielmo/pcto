92537f2154f430dbd2e3fc0525b2ee453cb0534a
VanettiGuglielmo <128370169+VanettiGuglielmo@users.noreply.github.com>
2023-03-21 16:40:30 +0100

    Add files via upload

---
+++ ex015.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int fattoriale(int n)
+
+{
+
+int y = 0;
+int z = 1;
+
+while(y < n)
+{
+ y = y + 1;
+ z = z * y;    
+}
+return(z);
+
+}
+
+int main()
+{
+
+    int numero;
+    printf("utente, inserisci un numero: ");
+    scanf("%d", &numero);
+
+    int f = fattoriale (numero);
+
+    printf("il fattoriale di %d è %d\n", numero, f);
+}
