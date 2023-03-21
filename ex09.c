2ed29ee51f311e8e82ab5caba7baaaec921a173c
VanettiGuglielmo <128370169+VanettiGuglielmo@users.noreply.github.com>
2023-03-21 10:28:32 +0100

    Add files via upload

--- ex09.c
+++ ex09.c
@@ -12,13 +12,14 @@ printf("inserire un anno");
 scanf("%f", &x);
     if(x< 1969)
     {
-        y=x+1969
+        y=x+1969;
         printf("sei nato prima dello sbarco\n");
-
     }
     else if(x> 1969)
-    y=x-1969
+    {
+        y=x-1969;
         printf("nato dopo lo  sbarco");
+    }
     else
     {
         printf(" sei nato nell'anno dello sbarco");
