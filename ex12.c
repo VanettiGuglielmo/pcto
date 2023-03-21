7be199e2c0e52767ebe5c73a50556d1e833afd3d
VanettiGuglielmo <128370169+VanettiGuglielmo@users.noreply.github.com>
2023-03-21 12:10:28 +0100

    Add files via upload

---
+++ ex12.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+    int numero;
+    
+    printf("utente, inserisci un numero");
+    scanf("Ad", &numero);
+
+    int divisore = nemero - 1
+    while (divisore)
+    {
+        if (numero% divisore ==0)
+        (
+            printf("non è un numero primo\n");
+        )
+        divisore--;
+    }
+
+    printf("è un numero peimo\n")
+
