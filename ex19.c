c1f9de9410e6df51de43cd94d31d17c2ddb805c4
VanettiGuglielmo <128370169+VanettiGuglielmo@users.noreply.github.com>
2023-03-22 11:43:09 +0100

    Add files via upload

---
+++ ex19.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    int y = 0;
+    int z = 0;
+    int k;
+    while(y == o)
+{
+    printf("digita il numero dei prezzi da aggiungere:");
+    scanf("%d", &x);
+;
+
+    if( x>0) 
+    {
+
+     z =  z + x;
+     if(z > 50)
+     {
+        k = (z * 5) * (100 / 85);
+        printf(" il prezzo ammonta %d\n", k)
+     }
+     else
+    {
+     if ( x > 30)
+
+     k = z* 5 * 100/90;
+     printf("il prezzo totale ammonta %d\n", k);
+    }
+    else 
+    {
+
+        k = z * 5;
+        printf("il prezzo tot ammonta %d\n", k);
+    }
+    }
+}
+else 
+
+k = z* 5;
+printf(" il prezzo tota ammonta a %d\n", k);
+
+}
+
+    
+
+
+    else if ( y > 50)
+    {
+
+        sconto2 = 15/100 * z;
+        printf("il costo è %d\n", )
+    }
+
+    
+
