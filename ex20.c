a7476a61fa2b861525a92f1783a96cd1888f3106
VanettiGuglielmo <128370169+VanettiGuglielmo@users.noreply.github.com>
2023-03-22 13:01:14 +0100

    Add files via upload

---
+++ ex20.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int x = 2;
+    int y = 4;
+    int potenza;
+    int radice;
+
+    potenza = pow( x, y);
+    radice = sqrt(y);
+    printf("la potenza di %d è %%d", x, y, potenza);
+    printf("la radice quadrata di %d è ")
+
+
+}
