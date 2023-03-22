d91d7e2595e892ec7ccceba5b43d08e60dba2ff4
VanettiGuglielmo <128370169+VanettiGuglielmo@users.noreply.github.com>
2023-03-22 09:50:00 +0100

    Add files via upload

---
+++ ex17.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+
+{
+    int x;
+    int y;
+    int z =0;
+    int k;
+    int j;
+
+z = 0
+    printf("ineserisciun numero:");
+    scanf("%d", &x);
+
+    while(x > 0)
+    {
+        printf("inserisci un numero positivo");
+    scanf("%d", &x);
+        y = y + 1;
+        z = z * y;
+
+    }
+    j = z / k;
+printf("la somma di tutti i numeri che hai inserito è: %d\n", z);
