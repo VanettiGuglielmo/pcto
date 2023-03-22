dac0d640aabc3635258ec35a744a47698019f0c7
VanettiGuglielmo <128370169+VanettiGuglielmo@users.noreply.github.com>
2023-03-22 09:25:28 +0100

    Add files via upload

--- ex16.c
+++ ex16.c
@@ -7,12 +7,13 @@ int main()
     int y =0;
     int z =0;
 
+z = 0
     printf("ineserisciun numero:");
     scanf("%d", &x);
 
-    while(y < x)
+    while(x > 0)
     {
-        printf("inserisci un numero:");
+        printf("inserisci un numero positivo");
     scanf("%d", &x);
         y = y + 1;
         z = z * y;
@@ -20,3 +21,4 @@ int main()
     }
 
 printf("la somma di tutti i numeri che hai inserito è: %d\n", z);
+}
