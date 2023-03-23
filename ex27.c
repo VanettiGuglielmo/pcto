b747aa2027d7d10af31be2d147250ee28f9cb934
VanettiGuglielmo <128370169+VanettiGuglielmo@users.noreply.github.com>
2023-03-23 10:37:06 +0100

    Add files via upload

---
+++ ex27.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+
+{
+    char a[5] = "ciao";
+    printf("%s", a);
+ 
+    char nome[] = "ciao";
+    printf("%s", a);
+
+    char *b = "ciao";
+    printf("%s",b);
+}
+
+
