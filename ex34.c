786f7d7eb945772b74bf3b16974eb61a7b4bbbb3
VanettiGuglielmo <128370169+VanettiGuglielmo@users.noreply.github.com>
2023-03-23 16:40:29 +0100

    Add files via upload

---
+++ ex34.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+int main()
+{
+    int x,y;
+    printf("inserisci le cordinate x y");
+    scanf("%d", &x);
+    scanf("%d", &y);
+    printf("x: %d\n", x);
+    printf("y: %d\n", y); 
+
+    if (y>0 && x>0)
+    {
+      printf("si trova nel primo quadrante");   
+    }
+    else if(y>0 && x<0)
+    {
+      printf ("si trova nel secondo quadrante");
+    }
+    else if (y<0 && x<0)
+    {
+      printf ("si trova nel terzo quadrante");
+    }
+    else if (y==0 && x!=0)
+    {
+        printf ("si trova sull0asse delle y");
+    }
+    else if (y!=0 && x==0)
+    {
+        printf (" si trova sull'asse delle x");
+    }
+    else if ( y==0 && x==0)
+    {
+        printf (" passa per l'origine");
+    }
+    else 
+    {
+        printf ("si trova nel quarto quadrante");
+    }
+}
+
+   
