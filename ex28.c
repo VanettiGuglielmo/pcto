70a7c657feb12dcd03d21d0da85857dd3a8fc283
VanettiGuglielmo <128370169+VanettiGuglielmo@users.noreply.github.com>
2023-03-23 11:27:12 +0100

    Add files via upload

---
+++ ex28.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+
+{
+    int f;
+    int i = 0;
+    char a[] = "parola";
+    while(a[i] != 0)
+    {
+        i = i + 1;
+    }
+
+    printf("la parola %s è fatta da %d lettere", a, i);
+
+}
+
+    
+ 
+    
