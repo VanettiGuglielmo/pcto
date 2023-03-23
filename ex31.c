9044bc4acf857e314c36569781e6de9ab498b36a
VanettiGuglielmo <128370169+VanettiGuglielmo@users.noreply.github.com>
2023-03-23 14:30:02 +0100

    Add files via upload

---
+++ ex31.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+
+void Giacobbe (char *x)
+{
+int i = 0;
+    while(x[i] != 0)
+    {
+        if(x[i] > 64 && x[i] < 91)
+        {
+            x[i] = x[i] + 32;
+        }
+        else
+        {
+            if(x[i] > 96 && x[i] < 123)
+            {
+                x[i] = x[i] - 32;
+            }
+        }
+        i = i + 1;
+    }
+printf("%s\n", x);
+}
+
+int main()
+{
+char x[] = "AlBerTo AngeLA";
+Giacobbe (x);
+}
